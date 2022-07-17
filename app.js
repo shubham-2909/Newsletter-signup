506bc17d908f251f7b7295d3b4ec0c94826e4233
sg976 <sg9769356@gmail.com>
2022-07-17 18:47:30 +0530

    create app

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.get("/",function (req,res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function (req,res) {
+  var fName = req.body.fName;
+  var lName = req.body.lName;
+  var email = req.body.email;
+  var data = {
+    members:[{
+      email_address:email,
+      status:"subscribed",
+      merge_fields:{
+        FNAME: fName,
+        LNAME: lName
+      }
+    }]
+  }
+ 
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url:" https://us14.api.mailchimp.com/3.0/lists/f4be9ab40b",
+    method:"POST",
+    body: jsonData,
+    headers:{
+      "Authorization":"Shuhbam1 7363dbe2df7a76f6fa40daf0d0360480-us14"
+    }
+  }
+
+  request(options,function (error,response,body) {
+        if(error){
+          res.sendFile(__dirname + "/failure.html");
+        }else{
+          if(response.statusCode == 200){
+            res.sendFile(__dirname + "/success.html");
+          }else{
+            res.sendFile(__dirname + "/failure.html");
+          }
+        }
+  })
+
+});
+
+app.post("/failure",function (req,res) {
+  res.redirect("/");
+})
+app.listen(process.env.PORT || 3000,function () {
+    console.log("Server is created at port 3000");
+})
+
+
+//7363dbe2df7a76f6fa40daf0d0360480-us14
+//f4be9ab40b
